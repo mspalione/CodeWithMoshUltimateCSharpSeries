1b7357159231f5b3e33e6d82d088363695261691
Melissa Spalione <melissaspalione86@gmail.com>
2020-05-29 02:55:42 -0400

    First two projects of the course.

---
+++ ControlFlowPractice/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace ControlFlowPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //1 - Write a program and ask the user to enter a number.
+        //   The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console.
+        //   Otherwise, display "Invalid". 
+        //  (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+
+            Console.WriteLine("Enter a number between 1 and 10.");
+            var number = int.Parse(Console.ReadLine());
+
+            if (number > 0 && number < 11)
+                Console.WriteLine("Valid.");
+            else
+                Console.WriteLine("Invalid");
+
+//        2- Write a program which takes two numbers from the console and displays the maximum of the two.
+            Console.WriteLine("Please enter a number.");
+            var num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a second number.");
+            var num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+                Console.WriteLine($"{num1} is the larger number.");
+            else
+                Console.WriteLine($"{num2} is the larger number.");
+
+//        3- Write a program and ask the user to enter the width and height of an image.
+//          Then tell if the image is landscape or portrait.            
+            Console.WriteLine("Please enter the width of the image.");
+            var width = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the height of the image.");
+            var height = int.Parse(Console.ReadLine());
+
+            var portrait = width < height;
+
+            var type = portrait ? "portrait" : "landscape";
+
+            Console.WriteLine($"The image is a {type}.");
+
+            //4 - Your job is to write a program for a speed camera.
+            //    For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic.
+            //    Write a program that asks the user to enter the speed limit.
+            //    Once set, the program asks for the speed of a car.
+            //    If the user enters a value less than the speed limit, program should display Ok on the console.
+            //    If the value is above the speed limit, the program should calculate the number of demerit points.
+            //    For every 5km / hr above the speed limit, 1 demerit points should be incurred and displayed on the console.
+            //    If the number of demerit points is above 12, the program should display License Suspended.
+            Console.WriteLine("Please enter the speed limit.");
+            var speedLimit = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The speed limit is {speedLimit}.\nWhat was the speed of the car?");
+            var carSpeed = int.Parse(Console.ReadLine());
+
+            if (carSpeed < speedLimit)
+                Console.WriteLine("Ok.");
+            else
+            {
+                var difference = carSpeed - speedLimit;
+                switch (difference)
+                {
+                    case 5:
+                        Console.WriteLine("1 demerit point.");
+                        break;
+                    case 10:
+                        Console.WriteLine("2 demerit points.");
+                        break;
+                    case 15:
+                        Console.WriteLine("3 demerit points.");
+                        break;
+                    case 20:
+                        Console.WriteLine("4 demerit points.");
+                        break;
+                    case 25:
+                        Console.WriteLine("5 demerit points.");
+                        break;
+                    case 30:
+                        Console.WriteLine("6 demerit points.");
+                        break;
+                    case 35:
+                        Console.WriteLine("7 demerit points.");
+                        break;
+                    case 40:
+                        Console.WriteLine("8 demerit points.");
+                        break;
+                    default:
+                        Console.WriteLine("License Suspended");
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ LoopPractice/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LoopPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
+            //    Display the count on the console.
+            Console.WriteLine("Program one: numbers divisible by three.");
+            var numbers = new List<int>();
+            for(var i = 0; i < 100; i++)
+            {
+                var number = i + 1;
+                if(number % 3 == 0)
+                numbers.Add(number);
+            }
+            Console.WriteLine($"The total number of numbers divisible by three between the numbers one and one hundred are {numbers.Count}");
+
+            //2 - Write a program and continuously ask the user to enter a number or "ok" to exit. 
+            //    Calculate the sum of all the previously entered numbers and display it on the console.
+            Console.WriteLine("\nProgram two: adding all entered numbers.");
+            var instructions = "Enter a number or 'ok' to exit.";
+            var add = 0;
+            while (true)
+            {
+                Console.WriteLine(instructions);
+                var response = Console.ReadLine();
+                if (response == "ok") break;
+                add += int.Parse(response);
+            }
+
+            Console.WriteLine($"I've been adding all your numbers. The total is {add}");
+
+            //3 - Write a program and ask the user to enter a number. 
+            //    Compute the factorial of the number and print it on the console. 
+            //    For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+            Console.WriteLine("\nProgram three: number factoral.\n Enter a number.");
+            var num = int.Parse(Console.ReadLine());
+            var fact = num;
+            for (var i = num - 1; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine($"\nFactorial of Given Number is: {num}! = {fact}.");
+
+
+            //4 - Write a program that picks a random number between 1 and 10.
+            //    Give the user 4 chances to guess the number.
+            //    If the user guesses the number, display “You won"; otherwise, display “You lost". 
+            //    (To make sure the program is behaving correctly, you can display the secret number on the console first.)
+            Console.WriteLine("\nProgram four: number guess.\nYou have four chances to guess the number I'm thinking of.\nFirst guess: ");
+            var rand = new Random();
+            var secretNumber = rand.Next(1, 10);
+            var guess = 0;
+            for(var i = 0; i < 4; i++)
+            {
+                guess = int.Parse(Console.ReadLine());
+                if(guess == secretNumber)
+                {
+                    Console.WriteLine($"You won! The secret number was {secretNumber}");
+                    break;
+                }
+
+                Console.WriteLine("Wrong. Guess again.");
+            }
+
+
+            //5 - Write a program and ask the user to enter a series of numbers separated by comma.
+            //    Find the maximum of the numbers and display it on the console. 
+            //    For example, if the user enters “5, 3, 8, 1, 4", the program should display 8."
+            Console.WriteLine("\nProgram five: comma separated numbers.\nEnter a series of numbers with each number separated by a comma.");
+            var numberInput = Console.ReadLine();
+            var numberArray = numberInput.Split(',');
+            var numberList = new List<int>();
+            foreach(var number in numberArray)
+            {
+                numberList.Add(int.Parse(number));
+            }
+            Console.WriteLine($"The largest number in your list is {numberList.Max()}");
+            
+        }
+    }
+}
