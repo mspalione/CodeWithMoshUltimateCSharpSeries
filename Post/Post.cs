8e94de9d2d417e71babac91779b37905d1347a88
Melissa Spalione <melissaspalione86@gmail.com>
2020-05-31 19:53:32 -0400

    Created private props with public methods getting and setting strings, ints, and datetime.

---
+++ Post/Post.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Post
+{
+    class Post
+    {
+        private string _title { get; set; }
+        private string _description { get; set; }
+        private string _created { get; set; }
+        private int _upvote { get; set; }
+        private int _downvote { get; set; }
+
+        public string TitlePost()
+        {
+            _title = Console.ReadLine();
+            return _title;
+        }
+
+        public string DescribePost()
+        {
+            _description = Console.ReadLine();
+            return _description;
+        }
+
+        public string CreatePost()
+        {
+            _created = DateTime.Now.ToShortDateString();
+            return _created;
+        }
+
+        public int Upvote(int upvote)
+        {
+            _upvote++;
+            return _upvote;
+        }
+
+        public int Downvote(int downvote)
+        {
+            _downvote--;
+            return _downvote;
+        }
+    }
+}
--- Post/Program.cs
+++ Post/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Post
+﻿using System;
+
+namespace Post
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Exercise 2: Design a StackOverflow PostDesign a class called Post.This class models a StackOverflow post.
+            //Exercise 2: Design a StackOverflow Post
+            //Design a class called Post.This class models a StackOverflow post.
             //It should have properties for title, description and the date/time it was created.
             //We should be able to up-vote or down-vote a post.We should also be able to see the current vote value.
             //    In the main method, create a post, up-vote and down-vote it a few times and then display the the current vote value.
@@ -16,6 +19,39 @@
             //    Many developers (even those with years of experience) tend to create classes that are purely data containers, 
             //and other classes that are purely behaviour (methods) providers.This is not object-oriented programming. 
             //    This is procedural programming. Such programs are very fragile.Making a change breaks many parts of the code.
+
+            var upvote = 0;
+            var downvote = 0;
+            var title = "";
+            var description = "";
+            var created = "";
+            var stop = false;
+            var input = "";
+            var post = new Post();
+            Console.WriteLine("Please submit your stack overflow post. What is your title?");
+            title = post.TitlePost();
+            Console.WriteLine($"The title of your post is '{title}'. \nWhat is your post?");
+            description = post.DescribePost();
+            created = post.CreatePost();
+            Console.WriteLine($"The description of your post is '{description}'. \nYour post was successfully created on {created}.\nTo upvote this post, please enter '+'. To downvote this post, please enter '-'. Type 'q' to quit.");
+            while (stop == false)
+            {
+                input = Console.ReadLine();
+                if (input == "q")
+                {
+                    stop = true;
+                    continue;
                 }
+                else if (input == "+") upvote = post.Upvote(upvote);
+                else if (input == "-") downvote = post.Downvote(downvote);
+                else
+                {
+                    Console.WriteLine("Invalid input. Please type '+', '-', or 'q'.");
+                    continue;
+                }
+
+                Console.WriteLine($"Upvotes: {upvote}\nDownvotes: {downvote}");
+            }
+        }
     }
 }
